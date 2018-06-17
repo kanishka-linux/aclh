84c1aa4c1769c3a71b5015a5eded53aabbe1f27d
kanishka-linux <kanishka.linux@gmail.com>
2018-06-17 12:48:13 +0530

    add test_aclh

--- tests/test_aclh.py
+++ tests/test_aclh.py
@@ -1,20 +1,20 @@
 """
 Copyright (C) 2018 kanishka-linux kanishka.linux@gmail.com
 
-This file is part of vinanti.
+This file is part of aclh.
 
-vinanti is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
+aclh is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-vinanti is distributed in the hope that it will be useful, 
+aclh is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
-along with vinanti.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with aclh.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
@@ -22,7 +22,7 @@ import sys
 import unittest
 from functools import partial
 
-        
+
 class TestAclh(unittest.TestCase):
     
     cmd = [
@@ -32,14 +32,11 @@ class TestAclh(unittest.TestCase):
         '--hdrs', 'User-Agent:Mozilla/5.0'
         ]
     
-    def test_get(self):
-        aclh = ACLH(self.cmd)
+    def test_log_level(self):
+        cmd = self.cmd + ['--no-print', '--log-level=debug']
+        aclh = ACLH(cmd)
         aclh.start()
-    
-    def test_print_links(self):
-        cmd = self.cmd + ['--print-links', '--no-print']
-        aclh = ACLH(self.cmd)
-        aclh.start()
+        
         
 if __name__ == '__main__':
     BASEDIR, BASEFILE = os.path.split(os.path.abspath(__file__))
